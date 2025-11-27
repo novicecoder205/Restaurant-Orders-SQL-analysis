ce19a85507e86b8915d57c5cc64df8034f104c83
novicecoder205 <warpick2000@gmail.com>
2025-11-28 02:03:36 +0700

    Objective 1: Explore the Items table.

--- Portfolio Project Restaurant data analysis 1.sql
+++ Portfolio Project Restaurant data analysis 1.sql
@@ -1,1 +1,11 @@
-USE restaurant_db;
+### Objective 1: Explore the Items Tabe:
+USE restaurant_db
+
+#1 View the menu_items tables and write a query to find the number of items on the menu:
+
+#2 What are the least/most expensive items on the menu?
+
+#3 How many Italian dishes are on the menu? what are the least and most expensive Italian dishes on the men?
+
+#4 How many dishes are in each category? what is the average dish price within each category?
+ 
