65451b68fa0e0419fac559b3f6045630972f585f
novicecoder205 <warpick2000@gmail.com>
2025-12-13 15:19:42 +0700

    Objective 2

--- Portfolio Project Restaurant data analysis 2.sql
+++ Portfolio Project Restaurant data analysis 2.sql
@@ -1,7 +1,8 @@
 -- 1. View the Order_details table:
-
+SELECT * FROM order_details;
 -- 2. What is the date range of the table?
-
+SELECT * FROM order_details
+ORDER BY order_date;
 -- 3. How many orders were made within this date range?
 
 -- 4. How many items were ordered within this date range?
