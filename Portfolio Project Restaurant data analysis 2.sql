44cf5b2093e00da5dccb734a236f064121524e85
novicecoder205 <warpick2000@gmail.com>
2025-12-13 15:12:42 +0700

    Create Portfolio Project Restaurant data analysis 2.sql

---
+++ Portfolio Project Restaurant data analysis 2.sql
@@ -0,0 +1,11 @@
+-- 1. View the Order_details table:
+
+-- 2. What is the date range of the table?
+
+-- 3. How many orders were made within this date range?
+
+-- 4. How many items were ordered within this date range?
+
+-- 5. Which orders had the most number of items?
+
+-- 6. How many orders had more than 12 items?
