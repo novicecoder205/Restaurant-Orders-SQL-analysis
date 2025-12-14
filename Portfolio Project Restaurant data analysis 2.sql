074b200e58dd28e19a7d18c63ae16251266a556a
novicecoder205 <warpick2000@gmail.com>
2025-12-14 15:16:32 +0700

    Update Objective 2

--- Portfolio Project Restaurant data analysis 2.sql
+++ Portfolio Project Restaurant data analysis 2.sql
@@ -9,7 +9,9 @@ SELECT MIN(order_date), MAX(order_date) FROM order_details;
 -- 3. How many orders were made within this date range?
 SELECT COUNT(DISTINCT order_id) FROM order_details;
 -- 4. How many items were ordered within this date range?
-
+SELECT COUNT(*) FROM order_details;
 -- 5. Which orders had the most number of items?
-
+SELECT order_id, COUNT(item_id) AS num_items
+FROM order_details
+GROUP by order_id;
 -- 6. How many orders had more than 12 items?
