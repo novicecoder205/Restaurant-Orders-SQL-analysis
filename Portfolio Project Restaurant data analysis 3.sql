db5af832c56878df11dd34412ab9b7be18914528
novicecoder205 <warpick2000@gmail.com>
2025-12-15 13:06:50 +0700

    Update to Objective 3

--- Portfolio Project Restaurant data analysis 3.sql
+++ Portfolio Project Restaurant data analysis 3.sql
@@ -1,1 +1,12 @@
-### Objective 3: Analyze Customer Behavior
+### Task 3: Analyze Customer Behavior
+
+-- 1. Combine the menu_items and order_details tables into a single table.alter
+
+-- 2. What were  the least and most ordered items? what categories were they in?
+
+-- 3. What were the top 5 orders that spent the most money?
+
+-- 4. View the details of the highest spend order, and gather insights from results
+
+-- 5. Views the details of the tops 5 highest spend order. 
+
