915230f3ffea95999c346b6d595461d2905615e1
novicecoder205 <warpick2000@gmail.com>
2025-12-16 13:24:37 +0700

    update on task 3

--- Portfolio Project Restaurant data analysis 3.sql
+++ Portfolio Project Restaurant data analysis 3.sql
@@ -16,6 +16,13 @@ ORDER by num_purchases DESC;
 
 -- 3. What were the top 5 orders that spent the most money?
 
+SELECT order_id, SUM(price) AS total_spend
+FROM order_details od LEFT JOIN menu_items mi
+     ON od.item_id = mi.menu_item_id
+GROUP BY order_id
+ORDER BY total_spend DESC
+LIMIT 5;
+
 -- 4. View the details of the highest spend order, and gather insights from results
 
 -- 5. Views the details of the tops 5 highest spend order. 
