bbd5f44c6b0cb7697ce5eb7d9868eec5c17ecfde
novicecoder205 <warpick2000@gmail.com>
2025-12-27 00:33:38 +0700

    Update Portfolio Project Restaurant data analysis 1.sql

--- Portfolio Project Restaurant data analysis 1.sql
+++ Portfolio Project Restaurant data analysis 1.sql
@@ -7,7 +7,8 @@ SELECT * FROM menu_items;
 # 2: Find the number of items on the menu?
 SELECT COUNT(*) FROM menu_items;
 
-# 3: What are the least and most expensive items on the menu?
+# 3: Find the most expensive and least expensive items on the menu. 
+
 SELECT * FROM menu_items
 ORDER BY price;
 
